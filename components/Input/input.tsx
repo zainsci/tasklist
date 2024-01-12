6df6407c7ea7fe596b21bc345e5782c68c4ca6a9
zainsci <60652827+zainsci@users.noreply.github.com>
2024-01-12 10:06:11 +0300

    fix(app): Fix input type errors & ui

--- components/Input/input.tsx
+++ components/Input/input.tsx
@@ -1,45 +1,31 @@
 import React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 
-const input = cva(
-	[
-		"block",
-		"rounded-md",
-		"border-2",
-		"border-neutral-300",
-		"dark:border-neutral-600",
-		"placeholder-gray-400",
-		"focus:ring-1",
-		"focus:ring-neutral-300",
-		"dark:focus:ring-neutral-600",
-		"focus:outline-none",
-		"disabled:opacity-40",
-	],
-	{
-		variants: {
-			size: {
-				small: ["text-sm", "w-40", "px-2", "py-1"],
-				medium: ["text-sm", "w-80", "px-4", "py-2"],
-				large: ["text-lg", "w-full", "px-6", "py-3"],
-			},
-		},
-		defaultVariants: {
-			size: "medium",
-		},
-	}
-)
+const input = cva([
+	"flex",
+	"w-full",
+	"px-3",
+	"py-2",
+	"text-sm",
+	"rounded-md",
+	"border-2",
+	"border-neutral-300",
+	"dark:border-neutral-600",
+	"placeholder-gray-400",
+	"focus-visible:ring-1",
+	"focus-visible:ring-neutral-300",
+	"dark:focus-visible:ring-neutral-600",
+	"focus:outline-none",
+	"disabled:opacity-40",
+	"disabled:cursor-not-allowed",
+])
 
 export interface InputProps
 	extends React.InputHTMLAttributes<HTMLInputElement>,
-		VariantProps<typeof input> {
-	invalid?: boolean
-}
+		VariantProps<typeof input> {}
 
-const Input: React.FC<InputProps> = ({
-	className,
-	size,
-	invalid,
-	...props
-}) => <input className={input({ className, size })} {...props} />
+const Input: React.FC<InputProps> = ({ className, size, ...props }) => (
+	<input className={input({ className })} {...props} />
+)
 
 export default Input
